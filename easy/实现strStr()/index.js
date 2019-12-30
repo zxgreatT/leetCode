139fc1b29801428866b5bf185ed3e656dd4ddd3e
tianzixuan <18019135296@163.com>
2019-12-30 17:51:36 +0800

    a

--- easy/实现strStr()/index.js
+++ easy/实现strStr()/index.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
+    var haystackArr = [...haystack]
+    var needleArr = [...needle]
+    for(var i=0;i<needleArr.length;i++) {
+        for(var j=0;j<haystackArr.length;j++) {
+            
+        }
+    }
 }
